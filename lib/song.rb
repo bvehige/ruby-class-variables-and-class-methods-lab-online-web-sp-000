8aed8d18f8b6792717f9741921849e81d65f78ef
Brian Vehige <brianvehige@gmail.com>
2020-03-03 05:53:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,9 @@ attr_accessor :name, :artist, :genre
 @@count = 0 
 
 def initialze(name, artist, genre)
-  @@name = name
-  @@artist
-  @@genre 
+  @@name = name 
+  @@artist = artist
+  @@genre = genre
   @@count += 1 
 end
 
